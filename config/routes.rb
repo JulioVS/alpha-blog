d3418d6c1f233ef5c7554b16b1c5d770415e9676
Julio Errecart <120730394+JulioVS@users.noreply.github.com>
2022-12-21 23:32:28 -0300

    Add About page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root 'pages#home'
+    # Mi controlador será "pages" y mi acción será "home" // JE.-
+  get 'about', to: 'pages#about'
+    # Para la pagina "/about" (GET de HTML), asocia el Get con una
+    # acción llamada 'about' de mi controlador de paginas // JE.- 
 end
