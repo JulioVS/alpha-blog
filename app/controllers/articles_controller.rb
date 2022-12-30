0fcebf4b3b3f02ea7da9aed6534bcb7e72b7afab
Julio Errecart <120730394+JulioVS@users.noreply.github.com>
2022-12-30 16:30:36 -0300

    Added List Articules ft

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ActionController::Base #ApplicationController
   def show
-    byebug
+    #byebug
+      # Debugger!!! Pasa el proceso en el navegador y me habilita
+      # la consola (donde ejecutamos "rails s") para debuggear
     @article = Article.find(params[:id])
       # El prefijo "@" hace que mi variable (aqui "article") se transforme
       # en una variable DE INSTANCIA, lo cual la hace disponible para
@@ -10,5 +12,9 @@ class ArticlesController < ActionController::Base #ApplicationController
       # complementando la accion GET de HTTP, aqui por ejemplo
       # ":id" seria el numero de articulo a buscar en la BD y luego 
       # exhibir en el navegador.-
+  end
+
+  def index
+    @articles = Article.all 
   end
 end
