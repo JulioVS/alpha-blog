a5dd81708ff9daa0177c6125d0001a3906580f85
Julio Errecart <120730394+JulioVS@users.noreply.github.com>
2022-12-30 12:54:31 -0300

    Added article show action

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,14 @@
+class ArticlesController < ActionController::Base #ApplicationController
+  def show
+    byebug
+    @article = Article.find(params[:id])
+      # El prefijo "@" hace que mi variable (aqui "article") se transforme
+      # en una variable DE INSTANCIA, lo cual la hace disponible para
+      # la vista HTML/ERB correspondiente y alli puedo extraer sus
+      # datos para exhibir!
+      # "params" es la estructura de hash que viene desde el navegador
+      # complementando la accion GET de HTTP, aqui por ejemplo
+      # ":id" seria el numero de articulo a buscar en la BD y luego 
+      # exhibir en el navegador.-
+  end
+end
