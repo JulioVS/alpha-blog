78cf3a1e972de69d7c29e7bc43b52431c576cbb0
Julio Errecart <julioer@me.com>
2023-10-30 14:02:39 -0300

    Added Category display and edit functionality L205

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,36 @@
 class CategoriesController < ApplicationController
   before_action :require_admin, except: [:index, :show] 
+  before_action :set_article, only: [:show, :edit, :update] 
 
   def show
-    @category = Category.find(params[:id]) 
+    # Paginado de los articulos de la categoria
+    @articles = @category.articles.paginate(page: params[:page], per_page: 5)
   end
 
   def index
-    @categories = Category.paginate(page: params[:page], per_page: 5) 
+    # Paginado de todas las categorías
+    # JE => Entre "Category" y ".paginate" agregué ".order(:name)" para que 
+    #       las categorías se desplieguen alfabéticamente y no por orden de
+    #       de creación.-
+    @categories = Category.order(:name).paginate(page: params[:page], per_page: 5) 
   end
 
   def new 
     @category = Category.new 
+  end
+
+  def edit
+    # 'set_article' se llama automaticamente para esta y otras acciones
+  end
+
+  def update
+    if @category.update(category_params)
+      flash[:notice] = "Category name succesfully updated"
+      redirect_to @category
+    else
+      flash[:alert] = "You ain't too bright, aren't ya...? FAIL!"
+      render 'edit'
+    end
   end
 
   def create
@@ -24,6 +44,10 @@ class CategoriesController < ApplicationController
   end
   
   private
+
+  def set_article
+    @category = Category.find(params[:id]) 
+  end
 
   def category_params
     params.require(:category).permit(:name) 
